e4b7cd0089afeb1359accef75b8550c92f9f68f3
mathias clausen <mathiasepc@msn.com>
2025-09-30 12:55:48 +0200

    Fjernet unused imports

--- src/main/java/org/example/kinoxp/models/SnackOrder.java
+++ src/main/java/org/example/kinoxp/models/SnackOrder.java
@@ -1,5 +1,22 @@
 package org.example.kinoxp.models;
 
+// !!! This is what lombok does !!!
+// If you don't want lombok, you can remove this import
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+// Here we create setter and getter methods
+@Setter
+@Getter
+
+// Here we create a constructor
+@AllArgsConstructor
+// Here we create a no-args constructor
+// If needed, you can add this constructor
+//@NoArgsConstructor
 public class SnackOrder {
     private Long id;
     private Integer quantity;
