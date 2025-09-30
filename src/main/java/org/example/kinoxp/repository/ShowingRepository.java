fcd28b449c227311746eb76c0deee89153fb32bb
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2025-09-30 16:00:16 +0200

    added showing repo and rest controller

---
+++ src/main/java/org/example/kinoxp/repository/ShowingRepository.java
@@ -0,0 +1,7 @@
+package org.example.kinoxp.repository;
+
+import org.example.kinoxp.models.Showing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowingRepository extends JpaRepository<Showing, Long> {
+}
