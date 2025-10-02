80cdf45ea991c81d156507a4304dc21746f745a5
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2025-10-02 11:24:38 +0200

    added showing model object

--- src/main/java/org/example/kinoxp/models/Showing.java
+++ src/main/java/org/example/kinoxp/models/Showing.java
@@ -7,10 +7,12 @@ import java.util.Set;
 // !!! This is what lombok does !!!
 // If you don't want lombok, you can remove this import
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 
 // Here we create setter and getter methods
@@ -21,17 +23,27 @@ import lombok.Setter;
 @AllArgsConstructor
 // Here we create a no-args constructor
 // If needed, you can add this constructor
-//@NoArgsConstructor
 
+@Entity
+@NoArgsConstructor
 public class Showing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Double price;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private LocalDate date;
     @ManyToOne
+    @JoinColumn(name = "movie_id")
     private Movie movie;
+    @ManyToOne
+    @JoinColumn(name = "screen_id")
     private Screen screen;
+    @ManyToOne
+    @JoinColumn(name = "period_id")
     private Period period;
+    @OneToMany(mappedBy = "showing", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Ticket> tickets;
+
 }
