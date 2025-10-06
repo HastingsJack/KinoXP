988cd8986ff6d0c7e0eb2b0558005879a4d174b0
HastingsJack <joha0005@stud.kea.dk>
2025-10-06 10:44:22 +0200

    deleted okhttp3 tests

--- src/test/java/org/example/kinoxp/repositories/MovieRepositoryTest.java
+++ src/test/java/org/example/kinoxp/repositories/MovieRepositoryTest.java
@@ -1,18 +1,13 @@
 package org.example.kinoxp.repositories;
 
-import org.example.kinoxp.models.Actor;
 import org.example.kinoxp.models.Movie;
-import org.example.kinoxp.models.Showing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -36,12 +31,4 @@ public class MovieRepositoryTest {
         List<Movie> movies = movieRepository.findAll();
         assertEquals(1, movies.size());
     }
-
-//    @Test
-//    public void testOkHttpOnActiveMovies() throws IOException {
-//        okhttp3.OkHttpClient client = new okhttp3.OkHttpClient();
-//        okhttp3.Request request = new okhttp3.Request.Builder().url("http://localhost:8080/active").build();
-//        okhttp3.Response response = client.newCall(request).execute();
-//        System.out.println(response.body());
-//    }
 }
